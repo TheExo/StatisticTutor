fbaea023687e11151c7d0472227d3bfe7dbff6d9
Ignacio Gomez <ignacio.gomez.chaverri@gmail.com>
2016-05-29 15:00:57 -0600

    Start of the application

---
+++ src/LogicPkg/PrograFinal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LogicPkg;
+
+import prografinal.InterfacePkg.MainMenuFrame;
+
+/**
+ *
+ * @author ignacio
+ */
+public class PrograFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainMenuFrame main = new MainMenuFrame();
+        main.setVisible(true);
+    }
+    
+}
